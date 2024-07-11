4e1ed9a7087b3b3b6340390d1302b5d60e49aa2c
pincha100 <113956174+pincha100@users.noreply.github.com>
2024-07-11 16:26:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#  1st program
+print((9 ** 0.5) * 5)
+#  2st program
+print(9.99 > 99.8 or 1000 != 1000.1)
+#  3st program
+print((1234 // 10) % 100 + (5678 // 10) % 100)
+#  4st program
+print(int(13.42 // 1) == int((42.13 - int(42.13)) * 100) or int(42.13 // 1) == int((13.42 - int(13.42)) * 100))
+
